be51b6c8b7bd7840b029859a6eeea8b2eff6d045
Russell Wong <russ.wong98@gmail.com>
2021-11-15 15:34:15 +0800

    creation on myftp.c(client) and included daemon initialization for myftpd.c

---
+++ myftp.c
@@ -0,0 +1,15 @@
+/*
+ *  File: myftp.c (client program)
+ *  Authors: Russell and TY
+ *  Date Start: 12 Nov 2021
+ *  Purpose: Project 2 - A Simple File Transfer Protocol
+ *  Description: a simple network protocol that can be used to download files from a remote site and to upload files to a remote site, and a client and a server programs that communicate using that protocol.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
--- myftpd.c
+++ myftpd.c
@@ -1,14 +1,34 @@
 /*
+ *  File: myftpd.c (server program)
  *  Authors: Russell and TY
  *  Date Start: 12 Nov 2021
- *  Purpose: Server program
+ *  Purpose: Project 2 - A Simple File Transfer Protocol
+ *  Description: a simple network protocol that can be used to download files from a remote site and to upload files to a remote site, and a client and a server programs that communicate using that protocol.
  */
 
 #include <stdio.h>
+#include <sys/types.h>
+#include "protocol.h"
 
 int main()
 {
-    printf("hello world!\n");
+    // creating daemon process
+    pid_t pid;
+
+    if((pid = fork()) < 0)
+    {
+        perror("fork() error");
+        exit(1);
+    }
+    else if(pid > 0) // parent process
+    {
+        exit(0);
+    }
+    
+    // child process continues
+    setsid(); // establish itself as session leader
+    chdir("/"); // changes working directory to root
+    umask(0); // clear file mode creation mask
 
     return 0;
 }
