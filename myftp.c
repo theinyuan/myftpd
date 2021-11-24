650c2d9fece9bc5855c7b687b7512ac4a31ca5bb
Russell Wong <russ.wong98@gmail.com>
2021-11-24 13:00:54 +0800

    enhancements to myftp

--- myftp.c
+++ myftp.c
@@ -13,6 +13,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
 
 #include "protocol.h"
 
@@ -23,16 +25,28 @@ int initClientProg(int *serverName, int *socketNum);
 // main begins here
 int main(int argc, char *argv[])
 {
-
+    startClientProg(argc, argv);
     return 0;
 }
 
 // functions implementation
+int startClientProg(int argumentCount, char *argumentValue[])
+{
+
+}
+
 int initClientProg(int *serverName, int *socketNum)
 {
     int sock;
+    struct sockaddr_in clientAddr;
     struct sockaddr_in serverAddr;
     struct hostent *hostport;
+
+    if((hostport = gethostbyname(serverName) == NULL))
+    {
+        printf("host %s not found.\n", serverName);
+        return(INVALID_HOST_NAME);
+    }
 
     if((sock = socket(AF_INET, SOCK_STREAM, 0) < 0))
     {
@@ -40,17 +54,23 @@ int initClientProg(int *serverName, int *socketNum)
         return(CREATE_SOCKET_FAILED);
     }
 
-    bzero((char *)&serverAddr, sizeof(serverAddr));
+    bzero((char *)&clientAddr, sizeof(clientAddr));
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(SERVER_FTP_PORT);
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    clientAddr.sin_port = 0;
 
-    if(bind(sock,(struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    if(bind(sock,(struct sockaddr *)&clientAddr, sizeof(clientAddr)) < 0)
     {
         perror("Unable to bind");
         return(BIND_FAILED);
     }
+
+    bzero((char *)&serverAddr, sizeof(serverAddr));
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = * (u_long *) hostport->h_addr;
+
 
     listen(sock, serverQueue);
     *socketNum = sock;
